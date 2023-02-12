f9fb192310257e1000e96154328806a8d87caa79
KartikeyTripathi7 <97465793+KartikeyTripathi7@users.noreply.github.com>
2023-02-13 02:12:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.querySelector("#message");
+    const yesButton = document.querySelector("#yes");
+    const noButton = document.querySelector("#no");
+  
+    message.textContent = "Will you be mine forever?";
+  
+    yesButton.addEventListener("click", function() {
+      alert("Congratulations! you are totally mine for today!!"   );
+    });
+  
+    noButton.addEventListener("click", function() {
+      alert("RHEGI TO FIRBHI MERI HI");
+    });
+  });
+  
